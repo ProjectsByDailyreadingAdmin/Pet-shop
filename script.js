0fe615cedd83ad8fb132b303852452734e1b1754
ProjectsByDailyreadingAdmin <113250876+ProjectsByDailyreadingAdmin@users.noreply.github.com>
2022-12-27 21:33:34 +0500

    nnn

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let menu = document.getElementById("menu")
 let nav = document.getElementById("nav")
 let header = document.getElementById("header")
+let user = document.getElementById("user")
+let login = document.getElementById("login")
 menu.onclick = () =>{
     nav.classList.toggle("active")
 }
-
-window.onscroll = () =>{
+user.onclick = () =>{
+        login.classList.toggle("active")
+    }
+    window.onscroll = () =>{
     if(window.scrollY > 0){
         header.classList.add("active")
     }
     else{
         header.classList.remove("active")
+        login.classList.remove("active")
     }
 }
 
